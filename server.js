99c7555cbca99db125559b02317a2f6ef43e19b3
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-14 11:40:29 +0200

    found user in database in login

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 import express from 'express';
 import { JobSource } from './models/JobSource.js';
+import { User } from './models/User.js';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import jwt from 'jsonwebtoken';
 
-const user = {
-    id: 1,
-    username: 'hans',
-    firstName: 'Hans',
-    lastName: 'Richter'
-};
-
 dotenv.config();
 
 const MONGODB_URI =
-    process.env.MONGODB_URI || 'mongodb://localhost/ej5-job-manager';
+	process.env.MONGODB_URI || 'mongodb://localhost/ej5-job-manager';
 
 mongoose.connect(MONGODB_URI, (err) => {
-    if (err) console.log(err);
+	if (err) console.log(err);
 });
 
 const app = express();
@@ -27,61 +21,69 @@ app.use(express.json());
 const port = process.env.PORT || 3045;
 
 const verifyToken = (req, res, next) => {
-    const bearerHeader = req.headers['authorization'];
-    if (typeof bearerHeader !== 'undefined') {
-        const bearer = bearerHeader.split(' ');
-        const bearerToken = bearer[1];
-        req.token = bearerToken;
-        next();
-    } else {
-        res.sendStatus(403);
-    }
+	const bearerHeader = req.headers['authorization'];
+	if (typeof bearerHeader !== 'undefined') {
+		const bearer = bearerHeader.split(' ');
+		const bearerToken = bearer[1];
+		req.token = bearerToken;
+		next();
+	} else {
+		res.sendStatus(403);
+	}
 };
 
 const decodeJwt = (token) => {
-    let base64Url = token.split('.')[1];
-    let base64 = base64Url.replace('-', '+').replace('_', '/');
-    let decodedData = JSON.parse(Buffer.from(base64, 'base64').toString('binary'));
-    return decodedData;
+	let base64Url = token.split('.')[1];
+	let base64 = base64Url.replace('-', '+').replace('_', '/');
+	let decodedData = JSON.parse(Buffer.from(base64, 'base64').toString('binary'));
+	return decodedData;
 }
 
 app.get('/', (req, res) => {
-    res.send('<h1>EJ2 Job Manager API</h1>');
+	res.send('<h1>EJ2 Job Manager API</h1>');
 });
 
 app.post('/maintain-login', verifyToken, (req, res) => {
-    jwt.verify(req.token, 'secretkey', (err, authData) => {
-        if (err) {
-            res.sendStatus(403);
-        } else {
-            const data = decodeJwt(req.token);
-            res.json({
-                user: data.user
-            });
-        }
-    });
+	jwt.verify(req.token, 'secretkey', (err, authData) => {
+		if (err) {
+			res.sendStatus(403);
+		} else {
+			const data = decodeJwt(req.token);
+			res.json({
+				user: data.user
+			});
+		}
+	});
 });
 
-app.post('/login', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    if (username === 'hans' && password === '123') {
-        jwt.sign({ user }, 'secretkey', { expiresIn: '20s' }, (err, token) => {
-            res.json({
-                user,
-                token
-            });
-        })
-    } else {
-        res.sendStatus(403);
-    }
+app.post('/login', async (req, res) => {
+	const username = req.body.username;
+	const password = req.body.password;
+	const user = await User.findOne({ username });
+	if (user === null) {
+		res.status(403).send('user not found');
+	} else {
+		res.send('FOUND USER');
+	}
+
+
+	// if (username === 'hans' && password === '123') {
+	//     jwt.sign({ user }, 'secretkey', { expiresIn: '20s' }, (err, token) => {
+	//         res.json({
+	//             user,
+	//             token
+	//         });
+	//     })
+	// } else {
+	//     res.sendStatus(403);
+	// }
 });
 
 app.get('/job-sources', async (req, res) => {
-    const jobSources = await JobSource.find();
-    res.status(200).json(jobSources);
+	const jobSources = await JobSource.find();
+	res.status(200).json(jobSources);
 });
 
 app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`);
+	console.log(`Listening on http://localhost:${port}`);
 });
